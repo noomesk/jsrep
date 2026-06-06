b65ecf0d8d20b0801279a3af0981aa5135574a71
noomesk <aplazasn@unal.edu.co>
2026-06-05 22:50:52 -0500

    tipado dinamico

---
+++ 01-tipos/04-tipado-dinamico.js
@@ -0,0 +1,9 @@
+let numero = 451;
+let nombre = "Angie";
+let verdadero = true;
+let undef;
+let nulo = null;
+
+nombre = 123; // el tipo de dato se puede cambiar, es decir, no es necesario declarar el tipo de dato al momento de crear la variable, esto es lo que se conoce como tipado dinámico
+
+console.log(nombre);
