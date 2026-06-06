e2e9bc9a9e1d8947466b6b3a92e12e36f00a5c4b
noomesk <aplazasn@unal.edu.co>
2026-06-05 23:17:36 -0500

    comentarios

---
+++ 01-tipos/05-comentarios.js
@@ -0,0 +1,18 @@
+// Comentario de una sola linea
+/* Comentario de varias lineas
+que puede ocupar varias lineas
+y es muy útil para explicar el código de una manera más detallada, o para dejar notas para otros 
+desarrolladores que puedan estar trabajando en el mismo proyecto, o incluso para uno mismo, 
+para recordar por qué se hizo algo de cierta manera, o para dejar ideas para futuras mejoras. */
+
+let numero =1984;
+
+console.log(numero); // esto es un comentario de una sola linea, y no afecta en nada el código, es decir, el código se ejecuta normalmente, pero el comentario es ignorado por el intérprete de JavaScript, y no tiene ningún efecto en el resultado final del programa.
+/**
+ * Si quiero aprovechar la funcionalidad de vscode, le doy (slash dos asteriscos) 
+ * y luego le doy enter, y automáticamente me crea un comentario de varias lineas, con una estructura predefinida, que me permite documentar mi código de una manera más clara y organizada, y además, me permite aprovechar la funcionalidad de vscode, que me muestra una vista previa del comentario cuando paso el mouse sobre la función o variable que estoy documentando, 
+ * lo que me ayuda a entender mejor el código y a recordar lo que hace cada función o variable sin tener que leer todo el código.
+ * entonces cada vez que le de enter, me va a crear una nueva linea con un asterisco, y puedo seguir escribiendo mi comentario de varias lineas, y cuando termine, simplemente cierro el comentario con otro asterisco y una barra, y listo, ya tengo mi comentario de varias lineas listo para ser utilizado.
+ * jajaj magiko amikos!!
+ * ang tqm ♥ 
+ */
