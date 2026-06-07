5c85b39bd449379005a25875a96f3b36123e1105
noomesk <aplazasn@unal.edu.co>
2026-06-07 01:22:38 -0500

    Op logicos

--- 02-operadores/04-logicos.js
+++ 02-operadores/04-logicos.js
@@ -5,3 +5,15 @@ console.log(true && true); // true
 console.log(true && false); // false
 console.log(false && true); // false
 console.log(false && false); // false  
+
+let mayorDeEdad = true;
+let suscrito = true;
+console.log("operador and", mayorDeEdad && suscrito); // true, porque ambas condiciones son verdaderas 
+
+//OR  (||): devuelve true si al menos una de las condiciones es verdadera, de lo contrario devuelve false
+console.log(true || true); // true
+console.log("operador or", mayorDeEdad || suscrito); //true, porque al menos una de las condiciones es verdadera
+
+//NOT
+console.log("operador not", !mayorDeEdad); // false, porque la negación de true es false
+//Como mayor de edad es true, pero NOT es la negación entonces lo invierte y ahora quda false. 
