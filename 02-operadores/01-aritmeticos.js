3c01c069891920dd6a593e31ee7c03ac7395a8c9
noomesk <aplazasn@unal.edu.co>
2026-06-06 23:52:40 -0500

    operadores aritmeticos

---
+++ 02-operadores/01-aritmeticos.js
@@ -0,0 +1,11 @@
+let a = 5;
+let b = 7;
+
+console.log(a + b, "adicion"); // operador de suma
+console.log(a - b, "resta"); // operador de resta
+console.log(a * b, "multiplicacion"); // operador de multiplicación
+console.log(a / b, "division"); // operador de división
+console.log(a % b, "modulo"); // operador de módulo, devuelve el residuo de la división entre a y b, es decir, el resultado de a dividido entre b, pero solo devuelve el residuo, no el cociente, por ejemplo, si a es 5 y b es 2, el resultado de a dividido entre b es 2.5, pero el resultado del operador de módulo es 1, porque el residuo de la división entre 5 y 2 es 1.
+console.log(a ** b, "potencia"); // operador de exponenciación O POTENCIA, devuelve el resultado de a elevado a la potencia de b, es decir, el resultado de multiplicar a por sí mismo b veces, por ejemplo, si a es 5 y b es 2, el resultado de a elevado a la potencia de b es 25, porque 5 multiplicado por sí mismo 2 veces es 25.
+
+
