e6b1d0bffa8ec5e941a303c03bb8dd834c462056
noomesk <aplazasn@unal.edu.co>
2026-06-06 22:57:06 -0500

    argumentos y parámetros en funciones

---
+++ 01-tipos/08-funciones.js
@@ -0,0 +1,16 @@
+function saludar (){
+    console.log("Quiubos");
+}
+
+//para llamar a la función, simplemente escribimos el nombre de la función seguido de paréntesis, es decir, saludar(), y esto ejecutará el código que se encuentra dentro de la función, en este caso, imprimirá "Quiubos" en la consola.
+saludar(); 
+
+//función de calculo de valores
+function suma(){
+    return 2+2;
+}
+
+//let resultado = suma(); 
+//console.log(resultado);
+
+console.log(suma()); // también podemos llamar a la función directamente dentro del console.log, sin necesidad de almacenar el resultado en una variable, y esto imprimirá el resultado de la función, que en este caso es 4, directamente en la consola.
---
+++ 01-tipos/09-argumentos.js
@@ -0,0 +1,24 @@
+// parametros (a) es un parametro dentro de la función:
+function suma(a, b){
+    return a+b;
+}
+
+
+//Ahora cuando llamamos a la función le pasamos un argumento (5) es un argumento que se le pasa a la función, y este argumento se asigna al parametro (a) dentro de la función, es decir, cuando llamamos a la función suma(5), el valor 5 se asigna al parametro a dentro de la función, y luego se ejecuta el código dentro de la función, que en este caso es return a+2, lo que devuelve el resultado de sumar 2 al valor de a, que en este caso es 5, por lo tanto, el resultado de la función suma(5) es 7.
+//let resultado = suma(5);
+//console.log(resultado); 
+
+
+//le vamos a pasar más parametros a la función: 
+//let resultado = suma(5, 6);
+//console.log(resultado);
+/* aqui no veriamos el resultado, xq solo estoy referenciando a = 5, y el segundo parametro
+no se esta utilizando dentro de la función, por lo tanto, el resultado de la función sigue 
+siendo 7, aunque le estemos pasando un segundo argumento, esto es porque la función solo 
+esta utilizando el primer parametro (a) y el segundo parametro no se esta utilizando dentro 
+de la función, por lo tanto, el resultado de la función sigue siendo 7, aunque le estemos 
+pasando un segundo argumento. */
+
+let resultado = suma(5, 6);
+console.log(resultado);
+console.log(typeof suma);
