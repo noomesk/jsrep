0acb41f70a5a5f23922dd8f700b6b8b6dc09c3eb
noomesk <aplazasn@unal.edu.co>
2026-06-07 00:01:54 -0500

    operadores aritmeticos2

--- 02-operadores/01-aritmeticos.js
+++ 02-operadores/01-aritmeticos.js
@@ -1,11 +1,23 @@
 let a = 5;
 let b = 7;
 
-console.log(a + b, "adicion"); // operador de suma
+
+/* console.log(a + b, "adicion"); // operador de suma
 console.log(a - b, "resta"); // operador de resta
 console.log(a * b, "multiplicacion"); // operador de multiplicación
 console.log(a / b, "division"); // operador de división
 console.log(a % b, "modulo"); // operador de módulo, devuelve el residuo de la división entre a y b, es decir, el resultado de a dividido entre b, pero solo devuelve el residuo, no el cociente, por ejemplo, si a es 5 y b es 2, el resultado de a dividido entre b es 2.5, pero el resultado del operador de módulo es 1, porque el residuo de la división entre 5 y 2 es 1.
 console.log(a ** b, "potencia"); // operador de exponenciación O POTENCIA, devuelve el resultado de a elevado a la potencia de b, es decir, el resultado de multiplicar a por sí mismo b veces, por ejemplo, si a es 5 y b es 2, el resultado de a elevado a la potencia de b es 25, porque 5 multiplicado por sí mismo 2 veces es 25.
 
+*/
 
+//incrementar
+console.log(++a); //incrementa en 1 el valor de a
+console.log(a++); //incrementa en 1 el valor de a, (pero aqui lo que hace es que primero lo muestra en consola 5, y luego aumenta el valor = 6)
+ // incrementar se puede usar de esas dos formas, antes o después de la variable
+ console.log(a);  
+
+ //decrementar del inglés decrement
+console.log(--b); //decrementa en 1 el valor de b
+console.log(b--); 
+console.log(b);
