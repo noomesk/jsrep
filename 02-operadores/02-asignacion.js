a1cf1833612ad7b2e8efbf90e57c37513b1853e5
noomesk <aplazasn@unal.edu.co>
2026-06-07 00:24:07 -0500

    operadores de asignacion

---
+++ 02-operadores/02-asignacion.js
@@ -0,0 +1,13 @@
+let a = 5;
+// let b = a + 5;  lo puedo hacer de una manera más corta asi:
+a = a + 5;
+
+a += 5; //esto me aumenta el valor en 5
+a -= 5; //esto me disminuye el valor en 5
+a *= 5; //esto me multiplica el valor en 5
+a /= 5; //esto me divide el valor en 5
+a %= 5; //esto me da el residuo de la división en 5
+a **= 5; //esto me da el resultado de elevar el valor a la potencia de 5
+console.log(a);
+ 
+
