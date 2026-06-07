e8fd666a02b7c0233065988628d216b0662fc081
noomesk <aplazasn@unal.edu.co>
2026-06-07 01:06:08 -0500

    op logicos1

---
+++ 02-operadores/04-logicos.js
@@ -0,0 +1,7 @@
+// AND, OR Y NOT
+
+//AND (&&): devuelve true si ambas condiciones son verdaderas, de lo contrario devuelve false
+console.log(true && true); // true
+console.log(true && false); // false
+console.log(false && true); // false
+console.log(false && false); // false  
