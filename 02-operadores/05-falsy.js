c092bea3d1e7427a3446221ec39167086af539cf
noomesk <aplazasn@unal.edu.co>
2026-06-08 23:06:05 -0500

    como usar falsy

--- 02-operadores/05-falsy.js
+++ 02-operadores/05-falsy.js
@@ -1,1 +1,15 @@
 //short circuit 
+
+//false
+//false
+//0
+//""
+//null
+//undefined
+//NaN
+
+//a los anteriores valores se les conoce como valores falsy, es decir, que se consideran falsos en un contexto booleano.
+//todos los demás valores se consideran verdaderos, es decir, truthy.
+let nombre = "";
+let username = nombre || "Anonimo";
+console.log(username); 
