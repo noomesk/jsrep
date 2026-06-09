dd8b18dafafb19047c312cafc2960cc960233a06
noomesk <aplazasn@unal.edu.co>
2026-06-08 23:18:25 -0500

    falsy en operador or y and

--- 02-operadores/05-falsy.js
+++ 02-operadores/05-falsy.js
@@ -10,6 +10,19 @@
 
 //a los anteriores valores se les conoce como valores falsy, es decir, que se consideran falsos en un contexto booleano.
 //todos los demás valores se consideran verdaderos, es decir, truthy.
-let nombre = "";
+let nombre = "Paquito feli";
 let username = nombre || "Anonimo";
 console.log(username); 
+
+//funciones
+function fn1() {
+    console.log("soy funcion 1");
+    return false;
+}
+
+function fn2() {
+    console.log("soy funcion 2");
+    return true;
+}
+
+let x = fn1() && fn2();
