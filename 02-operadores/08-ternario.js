2570f8271a2c9357bf109fff2ead97e936d9dcf8
noomesk <aplazasn@unal.edu.co>
2026-06-09 22:42:57 -0500

    op ternario

---
+++ 02-operadores/08-ternario.js
@@ -0,0 +1,9 @@
+// expresion ? "si es true" : "si es false"
+
+let edad = 25; 
+let acceso = edad  > 18 ? "Permitir ingreso" : "No puede ingresar";
+console.log(acceso);
+
+/* Funciona similar a "if", donde si la condición que se idicó anteriormente evalua en True:
+entonces nos devuelve: "Permitir ingreso", y si evalua en false, nos devuelve
+"No puede ingresar"
