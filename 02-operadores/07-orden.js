5ef543c666f90270dc50b8faa3f365752f225666
noomesk <aplazasn@unal.edu.co>
2026-06-09 22:29:15 -0500

    priorización de op

--- 02-operadores/07-orden.js
+++ 02-operadores/07-orden.js
@@ -1,2 +1,7 @@
 let resultado = 8/2*(2+2);
 console.log(resultado);
+//Ahora si quisiera cambiar el orden de respuesta y q en vez de 16 me de 1, uso parentesis:
+
+/* let resultado = 8/(2*(2+2));
+console.log(resultado); */ 
+//agrego paréntesis alrededor de la operación que quiero que se ejecute primero
