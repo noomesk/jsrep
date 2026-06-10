3e77c2ac14810955421ecedd625333f0b15c05a0
noomesk <aplazasn@unal.edu.co>
2026-06-09 23:48:42 -0500

    estructura del while

---
+++ 03-control-flujo/03-while.js
@@ -0,0 +1,11 @@
+// Lógica: Para poder imprimir hasta 10. ¿Cuáles son los números pares?
+
+let i = 0;
+while(i < 10){
+    console.log(i);
+    i++;
+}
+
+console.log("Fuera del while"); 
+
+//es una función iterativa y cuando la condición ya no se cumple se roompe el loop!
