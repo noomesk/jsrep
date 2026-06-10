196e1972b010b0bb9f4ed36c4156888459e77291
noomesk <aplazasn@unal.edu.co>
2026-06-09 23:55:20 -0500

    while: ejercicio numeros pares

--- 03-control-flujo/03-while.js
+++ 03-control-flujo/03-while.js
@@ -2,10 +2,13 @@
 
 let i = 0;
 while(i < 10){
-    console.log(i);
-    i++;
-}
+    if(i % 2 == 0){ //si le sacamos el modulo a i en base a 2 y da 0, significa que estamos trabajando con un # par
+        console.log("Número par", i);
+    }
+    i++; //esta condición siempre debe ir fuera de los if, ya que si por alguna razon esta cond no se cumpliera, entrariamos en un buble infinito
 
+}
+       
 console.log("Fuera del while"); 
 
 //es una función iterativa y cuando la condición ya no se cumple se roompe el loop!
