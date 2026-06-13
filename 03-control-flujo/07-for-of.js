4741691c952b238033927941f107ef3b8586442b
noomesk <aplazasn@unal.edu.co>
2026-06-12 23:51:59 -0500

    for of con while

--- 03-control-flujo/07-for-of.js
+++ 03-control-flujo/07-for-of.js
@@ -7,3 +7,10 @@ let animals = ["gatito", "perrito", "serpiente"];
 for (let animal of animals) {
     console.log(animal);
 }
+
+//otra forma de hacerlo, digamos más manual con while: 
+let i = 0; 
+while (i < animals.length) {
+    console.log(animals[i]);
+    i++;
+}
