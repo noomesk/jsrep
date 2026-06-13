060f4038b2c0a3ab849307adaa6dd8cc71d05bb3
noomesk <aplazasn@unal.edu.co>
2026-06-13 18:02:17 -0500

    for in parte 1

---
+++ 03-control-flujo/08-for-in.js
@@ -0,0 +1,10 @@
+let user = {
+    id: 101100010,
+    name: "Paquito",
+    age: 25,
+};
+
+for (let prop in user){ //prop es propiedad, en codigo de otros se puede leer asi
+    //luego la palabra reservada de "in" y alado hay que indicarle el objeto al cual le quiero iterar sus propiedades
+    console.log(prop);
+}
