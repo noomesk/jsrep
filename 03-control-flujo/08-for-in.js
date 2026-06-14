9cc091ad4490e49bbca0a5c4a0eb116802ce2b28
noomesk <aplazasn@unal.edu.co>
2026-06-13 20:24:06 -0500

    para mostrar propiedad y valores mejor usar: for of

--- 03-control-flujo/08-for-in.js
+++ 03-control-flujo/08-for-in.js
@@ -6,5 +6,10 @@ let user = {
 
 for (let prop in user){ //prop es propiedad, en codigo de otros se puede leer asi
     //luego la palabra reservada de "in" y alado hay que indicarle el objeto al cual le quiero iterar sus propiedades
-    console.log(prop);
+    console.log(prop, user[prop]);
+}
+
+let animals = ["gatito", "perrito", "serpiente"];
+for (let indice in animals) {
+    console.log(indice, animals[indice]); 
 }
