7e4faccbbea260d126e2d702908c321a4fb40a61
noomesk <aplazasn@unal.edu.co>
2026-06-13 23:15:39 -0500

    ejercicio1

---
+++ 04-ejercicios/01.js
@@ -0,0 +1,8 @@
+//ejercicio 1: construir una función que recibe 2 parametros y saber cuál de esos dos va a ser el número mayor.
+function cualEsMayor(a, b) {
+
+}
+
+let mayor = cualEsMayor(10,5);
+
+console.log(mayor);
