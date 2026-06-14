2647147b166e7a5ad4de281734815102ca0e5f1d
noomesk <aplazasn@unal.edu.co>
2026-06-14 00:32:01 -0500

    Solución ejercicio1

--- 04-ejercicios/01.js
+++ 04-ejercicios/01.js
@@ -1,8 +1,25 @@
 //ejercicio 1: construir una función que recibe 2 parametros y saber cuál de esos dos va a ser el número mayor.
-function cualEsMayor(a, b) {
-
+/*function cualEsMayor(a, b) {
 }
 
 let mayor = cualEsMayor(10,5);
 
-console.log(mayor);
+console.log(mayor); */
+
+// si intento imprimir no me devuelve nada, solucionar.
+//SOLUCIÓN, primero pseudocod:
+/*si a es mayor que b
+    devolver a
+si no
+    devolver b */ //eso es else if
+
+function cualEsMayor(a, b) {
+if (a > b) {
+    return a; 
+} else {
+    return b; 
+}
+}
+
+let mayor = cualEsMayor(10,5);
+    console.log("El número mayor es: ", mayor);
