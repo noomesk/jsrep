60099c903a5a4998e0239f6d67e62f47026c4b8b
noomesk <aplazasn@unal.edu.co>
2026-06-13 22:12:45 -0500

    intrsucción break

--- 03-control-flujo/09-break-continue.js
+++ 03-control-flujo/09-break-continue.js
@@ -2,8 +2,10 @@ let i = 0;
 while (i < 6) { 
     i++;
     if (i === 2) {
-        continue;
+        continue;  // al ejecutarse se salta al inicio del while y no ejecuta el console.log cuando se cumple la condición de i===2. 
+    } 
+        if (i === 4) {
+        break; //rompe el ciclo cuando i = 4
     }
-
     console.log(i);
 }
