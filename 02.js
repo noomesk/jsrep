3f748918002fdd018ff8075fce3f3900e61427dc
noomesk <aplazasn@unal.edu.co>
2026-06-14 21:22:12 -0500

    Ejercicio2

---
+++ 02.js
@@ -0,0 +1,16 @@
+/**
+ * EJERCICIO 2
+ * Nombre: ancho x alto 
+ * 8K 7680 x 4320
+ * 4K 3840 x 2160
+ * WQHD 2560 x 1440
+ * FHD 1920 x 1080 
+ * HD 1280  x 720
+ */
+
+function nombreResolucion(ancho, alto) {
+
+}
+
+let nombre = nombreResolucion(1366, 768);
+console.log(nombre); 
