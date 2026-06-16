152e49a607993bd2996e8ef45ea830e5595ac790
noomesk <aplazasn@unal.edu.co>
2026-06-16 14:27:10 -0500

    Ejercicio 03 planteamiento

---
+++ 03.js
@@ -0,0 +1,22 @@
+/** 
+ * Consatruir una funciòn que me permita obtener un elemento por su ìndice. Nosotros vamos a tener que pasarle el array
+ * y tambièn vamos a tener que pasarle el ìndice (viene con un pequeño truco): El valor que nosotros le pasemos como ìndice, no puede ser 
+ * menor que cero y el elemento tiene que existir en el array. En el caso de que le pase màs me tiene que indicar que el ìndice no
+ * es vàlido 
+ */
+
+function getbyIdx(arr, idx) {
+    if (idx > 0) {
+        return "indice no vàlido"; 
+    } else if (idx (no sè como poner valor inexistente...)) {
+        return "indice no vàlido"; 
+    } else {
+        return getbyIdx;  
+    }
+
+}
+
+let resultado = getbyIdx([1, 2], 1);
+console.log(resultado);
+
+//revisar array
