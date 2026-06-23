03d55de08165fbea84344d1ea0bb946f9285a24a
noomesk <aplazasn@unal.edu.co>
2026-06-23 00:48:25 -0500

    Ejercicio 4 resuelto, loop for

---
+++ 04-ejercicios/04.js
@@ -0,0 +1,17 @@
+//escribir un algoritmo que imprima números impares, pero del 0 al 10
+//usar un loop
+
+function impar() {
+ 
+    for (let i = 0; i <= 10; i++) {
+
+       if (i % 2 != 0 ) {
+           console.log("impar:", i);
+        }
+    }
+}
+
+impar();
+
+
+//wuuuuuju, q wen for amiga :3
