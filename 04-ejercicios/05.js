719e9aa2de721afae981cbb867d6a521b982f8ba
noomesk <aplazasn@unal.edu.co>
2026-06-24 23:59:08 -0500

    ejercicio 5 planteamiento

---
+++ 04-ejercicios/05.js
@@ -0,0 +1,11 @@
+/*ejercicio 2:47 Crear un algoritmo que devuelva un número
+menor y mayor de un array */
+
+let array = [2,5,7,15,-5,-100,55 ];
+
+function getMenorMayor(arr) {
+    //...
+}
+
+let numeros = getMenorMayor(array)
+console.log(numeros);
